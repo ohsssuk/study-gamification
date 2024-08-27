84f27e5d07dc10a6d481f610146f98b6dcf5c7a6
ohsssuk <ohsssuk@naver.com>
2024-08-28 01:17:51 +0900

    이미지 경로 문제 fix

--- components/coonyang/Loading.tsx
+++ components/coonyang/Loading.tsx
@@ -5,10 +5,7 @@ export default function Loading({ text = "젤리 식히는 중..." }: LoadingPro
   return (
     <div id="during_loading" className="loading-state">
       <div className="foot-loading">
-        <img
-          src="https://crcf.cookatmarket.com/assets/mobile/img/coonyang/foot.png"
-          alt={text}
-        />
+        <img src="/imgages/coonyang/foot.png" alt={text} />
         {text ? <p>{text}</p> : null}
       </div>
     </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
-
 const isProduction = process.env.NODE_ENV === "production";
+const productionURL = "https://ohsssuk.github.io";
+const productionPath = "/study-gamification";
 
 const nextConfig = {
   output: "export",
-  basePath: "/study-gamification",
-  assetPrefix: "/study-gamification",
+  basePath: isProduction ? productionURL : "",
+  assetPrefix: isProduction ? productionPath : "",
 };
 
 export default nextConfig;
