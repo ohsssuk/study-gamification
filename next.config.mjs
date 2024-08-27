77b4eb9019a4fa4a011a6564cf2243c6d51bfe91
ohsssuk <ohsssuk@naver.com>
2024-08-28 00:00:03 +0900

    next config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  // basePath 명시
+  basePath: isProduction ? "/study-gamification" : "",
+  assetPrefix: isProduction
+    ? "https://ohsssuk.github.io/study-gamification"
+    : "",
+};
 
 export default nextConfig;
