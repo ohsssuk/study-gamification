744ca0b7dc8e9520bb927340058c68f0290ef4e9
ohsssuk <ohsssuk@naver.com>
2024-08-28 01:51:42 +0900

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === "production";
 const productionPath = "/study-gamification";
+const productionURL = "https://ohsssuk.github.io/study-gamification/";
 
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true, // 이미지 최적화 비활성화
   },
-  basePath: productionPath,
-  assetPrefix: productionPath,
+  basePath: "",
+  assetPrefix: "",
 };
 
 export default nextConfig;
