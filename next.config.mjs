011c1ac80b73af6e1f659595bb2963a99f376fb4
raphy <ohsssuk@cookat.co>
2024-08-28 14:14:27 +0900

    웹패 설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === "production";
+const basePath = isProduction ? "/study-gamification" : "";
 
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true, // 이미지 최적화 비활성화
-    loader: "default",
-    path: isProduction ? "/study-gamification/" : "/",
   },
-  basePath: isProduction ? "/study-gamification" : "",
-  assetPrefix: isProduction ? "/study-gamification/" : "",
+  basePath: isProduction ? basePath : "",
+  assetPrefix: isProduction ? `${basePath}/` : "",
+  webpack(config) {
+    if (isProduction) {
+      config.output.publicPath = `${basePath}/`;
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
