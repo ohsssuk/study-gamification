c499418616133d7e8dd29ea165aac9cfd2177eba
raphy <ohsssuk@cookat.co>
2024-08-28 15:05:16 +0900

    DNS 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === "production";
-const basePath = isProduction ? "/study-gamification" : "";
 
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true, // 이미지 최적화 비활성화
   },
-  basePath: isProduction ? basePath : "",
-  assetPrefix: isProduction ? `${basePath}/` : "",
 };
 
 export default nextConfig;
